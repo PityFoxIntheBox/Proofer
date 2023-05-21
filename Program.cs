2c26db4275f8950a11870cc31aaa455704736603
Fox <email.com>
2023-05-21 23:34:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proofer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coding code = new Coding();
+            code.Action(9);
+        }
+    }
+}
